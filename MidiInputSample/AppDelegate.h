66ac2502b51416c22f178b9e8ba014065e91ac0e
Hector Urtubia <urtubia@bigrobotstudios.com>
2013-11-30 20:29:27 -0800

    Initial Commit

---
+++ MidiInputSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MidiInputSample
+//
+//  Created by hector urtubia on 11/30/13.
+//  Copyright (c) 2013 Big Robot Studios, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MidiInputSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MidiInputSample
+//
+//  Created by hector urtubia on 11/30/13.
+//  Copyright (c) 2013 Big Robot Studios, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
